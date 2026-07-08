7b9717c45018181281a7941d1a4eb88ab13df39f
sundaram20 <113882692+sundaram20@users.noreply.github.com>
2026-07-08 14:11:07 +0530

    db class changes

--- phplib/class.database.php
+++ phplib/class.database.php
@@ -1,143 +1,262 @@
 <?php
 
-class DbConnect {
+class DbConnect 
 
-    private $host;
-    private $user;
-    private $password;
-    private $database;
-    private $port;
-    private $persistent;
-    private $conn = null;
-    private $result = false;
-    private $error_reporting;
+{	
 
-    // Constructor
-    public function __construct(
-        $dbhost,
-        $dbuser,
-        $dbpass,
-        $dbname,
-        $dbport = 3306,
-        $error_reporting = true,
-        $persistent = false
-    ) {
-        $this->host = $dbhost;
-        $this->user = $dbuser;
-        $this->password = $dbpass;
-        $this->database = $dbname;
-        $this->port = $dbport;
-        $this->error_reporting = $error_reporting;
-        $this->persistent = $persistent;
-    }
+	/* Member Variables */
 
-    // Open connection
-    public function open() {
 
-        if ($this->persistent) {
-            $this->conn = mysqli_connect(
-                "p:" . $this->host,
-                $this->user,
-                $this->password,
-                $this->database,
-                $this->port
-            );
-        } else {
-            $this->conn = mysqli_connect(
-                $this->host,
-                $this->user,
-                $this->password,
-                $this->database,
-                $this->port
-            );
-        }
 
-        if (!$this->conn) {
-            if ($this->error_reporting) {
-                die("MySQL Connection Failed: " . mysqli_connect_error());
-            }
-            return false;
-        }
+	var $host = ''; 
 
-        return true;
-    }
+	var $user = ''; 
 
-    // Close connection
-    public function close() {
-        if ($this->conn) {
-            return mysqli_close($this->conn);
-        }
-        return false;
-    }
+	var $password = ''; 
 
-    // Run query
-    public function query($sql) {
-        $this->result = mysqli_query($this->conn, $sql);
-        return ($this->result !== false);
-    }
-    function query2($sql) {
+	var $database = ''; 
+
+	var $persistent = false; 
+
+	var $conn = NULL; 
+
+	var $result = false; 
+
+	var $error_reporting = false; 
+
+	
+
+	/* Constructor */
+
+
+
+	function DbConnect ($dbhost,$dbuser,$dbpass,$dbname, $error_reporting=true, $persistent=false) {
+
+		$this->host = $dbhost; 
+
+		$this->user = $dbuser; 
+
+        	$this->password =$dbpass; 
+
+		$this->database = $dbname; 
+
+        	$this->persistent = $persistent; 
+
+        	$this->error_reporting = $error_reporting; 
+
+	} 
+
+	
+
+	/* Member Functions */
+
+
+
+	/* Open Database Connection */
+
+
+
+	function open() { 
+
+		/* Connecting to Db Host */
+
+		if ($this->persistent) {
+
+			$this->conn = mysql_pconnect($this->host, $this->user, $this->password); 
+
+		} else {
+			$this->conn=	mysqli_connect($this->host, $this->user, $this->password,$this->database); 
+//			$this->conn = mysql_connect($this->host, $this->user, $this->password); 
+//mysqli_select_db($this->conn,$this->database);
+		  }             
+
+                   
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit;
+}
+        	if (!$this->conn) {
+
+			print "Cannot connect to Database Host: ". $this->host;
+
+			return false; 
+
+		} 
+
+                   
+
+	        /* Select DB */ 
+
+		if (@!mysqli_select_db($this->conn,$this->database)) {
+
+			print "Cannot select Database: ". $this->database;
+
+			return false; 
+
+		} 
+
+		return true; 
+
+	}  
+
+	
+
+	/* Close the connection */ 
+
+               
+
+	function close() {
+
+		return (@mysqli_close($this->conn)); 
+
+	} 
+
+    
+
+	/* Report Error */ 
+
+                 
+
+	function error() {
+
+		if ($this->error_reporting) {
+
+			return (mysqli_error($this->conn)) ; 
+
+		}           
+
+	} 
+
+	
+
+	/* Execute Query */
+
+
+
+	function query($sql) {
+
+		$this->result = mysqli_query($this->conn,$sql); 
+
+		return($this->result != false);         
+
+	} 
+	
+	
+	function query2($sql) {
 
 		$this->result = @mysqli_query($this->conn, $sql); 
 
 		return($this->result != false);         
 
 	} 
-    
-    // Number of rows
-public function num_rows() {
-    return mysqli_num_rows($this->result);
-}
-public function num_rows2($rss) {
-    return mysqli_num_rows($rss);
-}
-
- 
-// Fetch as object
-public function fetch_object() {
-    return mysqli_fetch_object($this->result);
-}
-public function fetch_object2($rss) {
-    return mysqli_fetch_object($rss);
-}
-public function fetch_obj2() {
-    return mysqli_fetch_object($rss);
-}
 
 
-public function fetch_array() {
-    return mysqli_fetch_array($this->result);
-}
-public function fetch_array2($rss) {
-    return mysqli_fetch_array($rss);
-}
+	
 
-// Fetch as array
-public function fetch_assoc() {
-    return mysqli_fetch_assoc($this->result);
-}
-
-public function fetch_assoc2($rss) {
-    return mysqli_fetch_assoc($rss);
-}  
-
-public function free_result() {
-    return mysqli_free_result($this->result);
-}
-  // Insert ID
-public function insert_id() {
-    return mysqli_insert_id($this->conn);
-}
-
-// Affected rows
-public function affected_rows() {
-    return mysqli_affected_rows($this->conn);
-}
+	/* Affected Rows for Updates & Deletes */
 
 
-    // Error
-    public function error() {
-        return $this->error_reporting ? mysqli_error($this->conn) : '';
-    }
-}
 
+	function affected_rows() {
+
+		return(@mysqli_affected_rows($this->conn, $this->conn)); 
+
+	} 
+
+	
+
+	/* Total Rows returned by Query */
+
+
+
+	function num_rows() {
+
+		return(@mysqli_num_rows($this->result)); 
+
+	} 
+	
+	
+	function num_rows2($rss) {
+
+		return(@mysqli_num_rows($rss)); 
+
+	} 
+
+	
+
+	
+
+	/* Fetch Resultset as Object */
+
+
+
+	function fetch_object() {
+
+		return(mysqli_fetch_object($this->result)); 
+
+	} 
+	
+	function fetch_object2($rss) {
+
+		return(mysqli_fetch_object($rss)); 
+
+	} 
+	
+	function fetch_obj2($rss) {
+
+		return(mysqli_fetch_object($rss)); 
+
+	} 
+
+	/* Fetch Resultset as Array */
+
+
+
+	function fetch_array() {
+
+		return(mysqli_fetch_array($this->result)); 
+
+	} 
+
+	function fetch_array2($rss) {
+
+		return(mysqli_fetch_array($rss)); 
+
+	} 
+
+
+	/* Fetch Resultset as Associate Array only */
+
+
+
+	function fetch_assoc() {
+
+		return(@mysqli_fetch_assoc($this->result)); 
+
+	}
+	
+	function fetch_assoc2($rss) {
+
+		return(@mysqli_fetch_assoc($rss)); 
+
+	}  
+	/* Free Resultset */
+
+	function free_result() {
+
+		return(@mysqli_free_result($this->result)); 
+
+	}  
+
+	
+
+	/* Free Resultset */
+
+	function insert_id() {
+		global $connNew;
+		return(mysqli_insert_id($connNew)); 
+
+	} 
+
+} 
 ?>
