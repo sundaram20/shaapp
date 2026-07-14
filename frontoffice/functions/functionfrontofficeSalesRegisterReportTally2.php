89c9535600483c5f34d3b6a926b55f8e0d1baefe
sundaram20 <113882692+sundaram20@users.noreply.github.com>
2026-07-14 14:05:54 +0530

    test

--- frontoffice/functions/functionfrontofficeSalesRegisterReportTally2.php
+++ frontoffice/functions/functionfrontofficeSalesRegisterReportTally2.php
@@ -634,8 +634,8 @@ $tax_percent = 0;
     }
    
 	//$doc_type = selectColumn(TBL_PURCH, 'doc_type', "WHERE id='{$record->id}' AND id_shop='{$id_shop}'");
-	//$tax_percent=$reservation->tax_percent;
-	//tax
+	$tax_percent=$reservation->tax_percent;
+	
 				$percentage_sgst	=round($percentage > 0 ? ($percentage / 2) : 0);
 				$percentage_cgst	=round($percentage > 0 ? ($percentage / 2) : 0);
 				$taxMethod_sgst='Tariff Sales';	
